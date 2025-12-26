dc1a9f80eb9cf7067801076b02e8c8925676aac6
Gilbert <arioadiyoso@gmail.com>
2025-12-26 10:02:31 +0700

    add materi form html

--- public/tamp/js/materi.js
+++ public/tamp/js/materi.js
@@ -95,3 +95,29 @@
                 console.log('Filters applied');
             });
         });
+
+
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                    colors: {
+                        primary: '#2563eb', // Blue 600
+                        slate: {
+                            50: '#f8fafc',
+                            100: '#f1f5f9',
+                            200: '#e2e8f0',
+                            300: '#cbd5e1',
+                            400: '#94a3b8',
+                            500: '#64748b',
+                            600: '#475569',
+                            700: '#334155',
+                            800: '#1e293b',
+                            900: '#0f172a',
+                        }
+                    }
+                }
+            }
+        }
